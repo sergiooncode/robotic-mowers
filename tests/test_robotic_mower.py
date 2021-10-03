866002f528068caa17cd14c06f40a4fd208e5537
Sergio Perez <sperez4mba@gmail.com.com>
2021-10-03 11:55:38 +0200

    Introduce variable for expected position

--- tests/test_robotic_mower.py
+++ tests/test_robotic_mower.py
@@ -6,11 +6,14 @@ def test_return_position_when_empty_instructions_received():
     initial_y = 2
     initial_orientation = "N"
     empty_instructions = ""
+
+    expected_position = "1 2 N"
+
     current_position = RoboticMower(
         x=initial_x, y=initial_y, orientation=initial_orientation
     ).execute(instructions=empty_instructions)
 
-    assert "1 2 N" == current_position
+    assert expected_position == current_position
 
 
 def test_return_position_when_move_instruction_received():
@@ -18,8 +21,11 @@ def test_return_position_when_move_instruction_received():
     initial_y = 2
     initial_orientation = "N"
     received_instructions = MOVE_COMMAND
+
+    expected_position = "1 3 N"
+
     current_position = RoboticMower(
         x=initial_x, y=initial_y, orientation=initial_orientation
     ).execute(instructions=received_instructions)
 
-    assert "1 3 N" == current_position
+    assert expected_position == current_position
