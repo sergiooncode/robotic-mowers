fae078f2cf6bae1a1468c13618c255b5a0adf665
Sergio Perez <sperez4mba@gmail.com.com>
2021-10-03 16:03:58 +0200

    Extract move method and in turn methods to move vertically and horizontally

--- src/robotic_mower.py
+++ src/robotic_mower.py
@@ -5,6 +5,10 @@ NORTH_ORIENTATION = "N"
 SOUTH_ORIENTATION = "S"
 EAST_ORIENTATION = "E"
 WEST_ORIENTATION = "W"
+ONE_GRID_POINT_UP = 1
+ONE_GRID_POINT_DOWN = -1
+ONE_GRID_POINT_RIGHT = -1
+ONE_GRID_POINT_LEFT = 1
 
 
 class RoboticMower:
@@ -17,15 +21,24 @@ class RoboticMower:
     def execute(self, instructions: str) -> Optional[str]:
         for instruction in list(instructions):
             if self.__is_move(instruction):
-                if self.__is_heading(NORTH_ORIENTATION):
-                    self.__y += 1
-                if self.__is_heading(SOUTH_ORIENTATION):
-                    self.__y -= 1
-                if self.__is_heading(EAST_ORIENTATION):
-                    self.__x -= 1
-                if self.__is_heading(WEST_ORIENTATION):
-                    self.__x += 1
+                self.__move()
         return self.__format_position()
+
+    def __move(self):
+        if self.__is_heading(NORTH_ORIENTATION):
+            self.__move_vertically(ONE_GRID_POINT_UP)
+        if self.__is_heading(SOUTH_ORIENTATION):
+            self.__move_vertically(ONE_GRID_POINT_DOWN)
+        if self.__is_heading(EAST_ORIENTATION):
+            self.__move_horizontally(ONE_GRID_POINT_RIGHT)
+        if self.__is_heading(WEST_ORIENTATION):
+            self.__move_horizontally(ONE_GRID_POINT_LEFT)
+
+    def __move_horizontally(self, step_size):
+        self.__x = self.__x + step_size
+
+    def __move_vertically(self, step_size):
+        self.__y = self.__y + step_size
 
     def __format_position(self):
         return f"{self.__x} {self.__y} {self.__orientation}"
