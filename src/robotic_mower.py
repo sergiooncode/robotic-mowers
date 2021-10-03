0f7c328a9e2306f6e4b162f0e6fdfda6096f9145
Sergio Perez <sperez4mba@gmail.com.com>
2021-10-03 13:59:54 +0200

    Extract methods to check instruction is move and heading

--- src/robotic_mower.py
+++ src/robotic_mower.py
@@ -16,16 +16,23 @@ class RoboticMower:
 
     def execute(self, instructions: str) -> Optional[str]:
         for instruction in list(instructions):
-            if instruction == MOVE_COMMAND:
-                if self.__orientation == NORTH_ORIENTATION:
+            if self.__is_move(instruction):
+                if self.__is_heading(NORTH_ORIENTATION):
                     self.__y += 1
-                if self.__orientation == SOUTH_ORIENTATION:
+                if self.__is_heading(SOUTH_ORIENTATION):
                     self.__y -= 1
-                if self.__orientation == EAST_ORIENTATION:
+                if self.__is_heading(EAST_ORIENTATION):
                     self.__x -= 1
-                if self.__orientation == WEST_ORIENTATION:
+                if self.__is_heading(WEST_ORIENTATION):
                     self.__x += 1
         return self.__format_position()
 
     def __format_position(self):
         return f"{self.__x} {self.__y} {self.__orientation}"
+
+    def __is_heading(self, orientation):
+        return self.__orientation == orientation
+
+    @staticmethod
+    def __is_move(instruction):
+        return instruction == MOVE_COMMAND
