9780baf3804d914c259dc6f011ae5e4ba8d880ab
Sergio Perez <sperez4mba@gmail.com.com>
2021-10-03 11:53:02 +0200

    Add test and refactors

--- src/robotic_mower.py
+++ src/robotic_mower.py
@@ -1,11 +1,19 @@
 from typing import Optional
 
+MOVE_COMMAND = "M"
+
 
 class RoboticMower:
+
     def __init__(self, x: int, y: int, orientation: str):
         self.__x = x
         self.__y = y
         self.__orientation = orientation
 
     def execute(self, instructions: str) -> Optional[str]:
-        return "1 2 N"
+        if instructions == MOVE_COMMAND:
+            self.__y += 1
+        return self.__format_position()
+
+    def __format_position(self):
+        return f"{self.__x} {self.__y} {self.__orientation}"
--- tests/test_robotic_mower.py
+++ tests/test_robotic_mower.py
@@ -1,4 +1,4 @@
-from src.robotic_mower import RoboticMower
+from src.robotic_mower import RoboticMower, MOVE_COMMAND
 
 
 def test_return_position_when_empty_instructions_received():
@@ -11,3 +11,15 @@ def test_return_position_when_empty_instructions_received():
     ).execute(instructions=empty_instructions)
 
     assert "1 2 N" == current_position
+
+
+def test_return_position_when_move_instruction_received():
+    initial_x = 1
+    initial_y = 2
+    initial_orientation = "N"
+    received_instructions = MOVE_COMMAND
+    current_position = RoboticMower(
+        x=initial_x, y=initial_y, orientation=initial_orientation
+    ).execute(instructions=received_instructions)
+
+    assert "1 3 N" == current_position
